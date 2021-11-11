f14ccb46fbc962b7a7b62cdead524b6be624ade8
Tony Chung <waiting33118@gmail.com>
2021-11-11 16:04:27 +0800

    feat(backend): add cors middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,13 @@
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
 import router from './routes';
 
 const app = express();
+const ORIGIN =
+  process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : '';
 
+app.use(cors({ origin: ORIGIN, credentials: true }));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(router);
