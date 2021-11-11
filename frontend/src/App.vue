07149b740d7fb333212069862eef2e51b81167ab
Tony Chung <waiting33118@gmail.com>
2021-11-11 14:05:03 +0800

    feat: create main page layout

---
+++ frontend/src/App.vue
@@ -0,0 +1,40 @@
+<script>
+import Navbar from './components/Navbar.vue';
+export default {
+  setup() {
+    return {};
+  },
+  components: {
+    Navbar
+  }
+};
+</script>
+
+<template>
+  <el-container>
+    <el-header>
+      <navbar />
+    </el-header>
+    <el-main>
+      <router-view />
+    </el-main>
+  </el-container>
+</template>
+
+<style lang="scss">
+* {
+  box-sizing: border-box;
+}
+
+html,
+body {
+  margin: 0;
+  padding: 0;
+}
+
+.el-header {
+  display: flex;
+  align-items: center;
+  box-shadow: 0px 3px 3px rgba(0, 0, 0, 0.3);
+}
+</style>
