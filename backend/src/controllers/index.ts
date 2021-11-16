a3d6e3212e68bdc508f97eee8d4cba835a3bddd9
Tony Chung <waiting33118@gmail.com>
2021-11-16 10:07:33 +0800

    feat: add user controller namespace

--- backend/src/controllers/index.ts
+++ backend/src/controllers/index.ts
@@ -1,9 +1,11 @@
 import restaurant from './restaurant';
 import category from './category';
 import region from './region';
+import user from './user';
 
 export default {
   restaurant,
   category,
-  region
+  region,
+  user
 };
