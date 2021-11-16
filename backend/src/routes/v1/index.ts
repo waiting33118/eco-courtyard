4b54f489b37fec2f814fda3ac5fbb70db1ad6072
Tony Chung <waiting33118@gmail.com>
2021-11-16 10:21:01 +0800

    feat: create user routes
    - /user/signup
    - /user/login
    - /user/profile
    - /user/logout

--- backend/src/routes/v1/index.ts
+++ backend/src/routes/v1/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
+import passport from '../../plugins/passport';
 import controllers from '../../controllers';
+import middlewares from '../../middlewares';
 const router = express.Router();
 
 router.get('/restaurant');
@@ -16,5 +18,23 @@ router.get('/region/:regionId', controllers.region.getRegion);
 router.post('/region', controllers.region.addRegion);
 router.put('/region/:regionId', controllers.region.editRegion);
 router.delete('/region/:regionId', controllers.region.deleteRegion);
+
+router.post('/user/signup', controllers.user.addUser);
+router.post(
+  '/user/login',
+  controllers.user.loginUser,
+  passport.authenticate('local'),
+  middlewares.auth.authSuccessResponse
+);
+router.get(
+  '/user/profile',
+  middlewares.auth.isAuthenticated,
+  controllers.user.getCurrentUser
+);
+router.post(
+  '/user/logout',
+  middlewares.auth.isAuthenticated,
+  middlewares.auth.logout
+);
 
 export default router;
