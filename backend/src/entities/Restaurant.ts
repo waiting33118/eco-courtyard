38d90621f1a400e9bd3ba741b36e13eca8177403
Tony Chung <waiting33118@gmail.com>
2021-11-21 20:58:26 +0800

    feat: create restaurant entity
    - relation to category table
    - relation to region table
    - relation to user table

---
+++ backend/src/entities/Restaurant.ts
@@ -0,0 +1,52 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  JoinColumn,
+  OneToOne,
+  ManyToOne
+} from 'typeorm';
+import { RestaurantCategory } from './RestaurantCategory';
+import { RestaurantRegion } from './RestaurantRegion';
+import { User } from './User';
+
+@Entity({ name: 'Restaurant' })
+export class Restaurant {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ type: 'varchar', nullable: false })
+  name!: string;
+
+  @Column({ type: 'varchar', nullable: false })
+  address!: string;
+
+  @Column({ type: 'varchar', name: 'start_time', nullable: false })
+  startTime!: string;
+
+  @Column({ type: 'varchar', name: 'close_time', nullable: false })
+  closeTime!: string;
+
+  @Column({ type: 'text', name: 'image_url', nullable: true })
+  imageUrl!: string;
+
+  @CreateDateColumn()
+  created_at!: Date;
+
+  @UpdateDateColumn()
+  updated_at!: Date;
+
+  @ManyToOne(() => RestaurantCategory)
+  @JoinColumn()
+  category!: RestaurantCategory;
+
+  @ManyToOne(() => RestaurantRegion)
+  @JoinColumn()
+  region!: RestaurantRegion;
+
+  @OneToOne(() => User, (user) => user.restaurant)
+  @JoinColumn()
+  owner!: User;
+}
