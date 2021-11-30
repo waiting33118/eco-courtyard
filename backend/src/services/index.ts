73f9616748886bda545058fb0f07e0591e3a8caa
Tony Chung <waiting33118@gmail.com>
2021-11-30 15:23:16 +0800

    feat: add cart in service namespace

--- backend/src/services/index.ts
+++ backend/src/services/index.ts
@@ -3,11 +3,13 @@ import regionService from './region';
 import userService from './user';
 import restaurantService from './restaurant';
 import cuisineService from './cuisine';
+import cartService from './cart';
 
 export {
   categoryService,
   regionService,
   userService,
   restaurantService,
-  cuisineService
+  cuisineService,
+  cartService
 };
